0a260a05a31be6469134adea5e5278efc77c8277
sameer2198 <ashutoshsameer@iitkgp.ac.in>
2017-03-18 00:11:54 +0530

    updated table structure

--- sihapp.sql
+++ sihapp.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 10, 2017 at 10:01 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.21
+-- Generation Time: Mar 17, 2017 at 07:40 PM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 7.0.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sihapp`
+-- Database: `sih`
 --
 
 -- --------------------------------------------------------
@@ -33,16 +33,17 @@ CREATE TABLE `nodes` (
   `quantity` int(11) NOT NULL,
   `parent` int(11) NOT NULL,
   `level_index` int(11) NOT NULL,
-  `is_home` tinyint(1) NOT NULL
+  `is_home` tinyint(1) NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `nodes`
 --
 
-INSERT INTO `nodes` (`no.`, `level`, `quality`, `quantity`, `parent`, `level_index`, `is_home`) VALUES
-(1, 1, 1, 1, 1, 1, 1),
-(2, 2, 2, 2, 2, 2, 0);
+INSERT INTO `nodes` (`no.`, `level`, `quality`, `quantity`, `parent`, `level_index`, `is_home`, `updated_at`) VALUES
+(1, 1, 1, 1, 1, 1, 1, '2017-03-17 18:38:14'),
+(2, 2, 2, 2, 2, 2, 0, '2017-03-17 18:38:14');
 
 -- --------------------------------------------------------
 
