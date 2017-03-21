a3b73b66d7f539225dc2d049ce35aa2cbb17aca5
Paras Chaudhary <paras7735@gmail.com>
2017-03-21 14:45:24 +0530

    some values added in database

--- sihapp.sql
+++ sihapp.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 20, 2017 at 09:46 AM
--- Server version: 10.1.10-MariaDB
--- PHP Version: 7.0.4
+-- Generation Time: Mar 21, 2017 at 10:13 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,12 +27,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `nodes` (
-  `no.` int(11) NOT NULL,
-  `level` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `level` int(255) NOT NULL,
+  `level_index` int(255) NOT NULL,
   `quality` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
   `parent` int(11) NOT NULL,
-  `level_index` int(11) NOT NULL,
   `is_home` tinyint(1) NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -41,9 +41,36 @@ CREATE TABLE `nodes` (
 -- Dumping data for table `nodes`
 --
 
-INSERT INTO `nodes` (`no.`, `level`, `quality`, `quantity`, `parent`, `level_index`, `is_home`, `updated_at`) VALUES
-(1, 1, 1, 1, 1, 1, 1, '2017-03-17 18:38:14'),
-(2, 2, 2, 2, 2, 2, 0, '2017-03-17 18:38:14');
+INSERT INTO `nodes` (`id`, `level`, `level_index`, `quality`, `quantity`, `parent`, `is_home`, `updated_at`) VALUES
+(0, 0, 0, 0, 0, 0, 0, '2017-03-20 18:04:44'),
+(1, 1, 0, 0, 0, 0, 0, '0000-00-00 00:00:00'),
+(2, 1, 1, 0, 0, 0, 0, '0000-00-00 00:00:00'),
+(3, 2, 0, 0, 0, 1, 0, '2017-03-20 18:05:12'),
+(4, 2, 1, 0, 0, 1, 0, '2017-03-20 18:05:16'),
+(5, 2, 2, 0, 0, 2, 0, '2017-03-20 18:05:21'),
+(6, 2, 3, 0, 0, 2, 0, '2017-03-20 18:05:31'),
+(7, 2, 4, 0, 0, 2, 0, '2017-03-20 18:05:34'),
+(8, 3, 0, 0, 0, 3, 0, '2017-03-20 18:05:53'),
+(9, 3, 1, 0, 0, 4, 0, '2017-03-20 18:05:59'),
+(10, 3, 2, 0, 0, 4, 0, '2017-03-20 18:06:02'),
+(11, 3, 3, 0, 0, 4, 0, '2017-03-20 18:06:22'),
+(12, 3, 4, 0, 0, 5, 0, '2017-03-20 18:06:26'),
+(13, 3, 5, 0, 0, 5, 0, '2017-03-20 18:06:28'),
+(14, 3, 6, 0, 0, 6, 0, '2017-03-20 18:06:31'),
+(15, 3, 7, 0, 0, 7, 0, '2017-03-20 18:06:35'),
+(16, 3, 8, 0, 0, 7, 0, '2017-03-20 18:06:38'),
+(17, 4, 0, 0, 0, 8, 0, '2017-03-20 18:21:08'),
+(18, 4, 1, 0, 0, 8, 0, '2017-03-20 18:21:11'),
+(19, 4, 2, 0, 0, 14, 0, '2017-03-20 18:21:32'),
+(20, 4, 3, 0, 0, 14, 0, '2017-03-20 18:21:37'),
+(21, 4, 4, 0, 0, 14, 0, '2017-03-20 18:21:39'),
+(22, 4, 5, 0, 0, 14, 0, '2017-03-20 18:21:42'),
+(23, 4, 6, 0, 0, 15, 0, '2017-03-20 18:21:54'),
+(24, 4, 7, 0, 0, 15, 0, '2017-03-20 18:21:56'),
+(25, 4, 8, 0, 0, 1, 0, '2017-03-20 18:22:21'),
+(26, 5, 0, 0, 0, 20, 0, '2017-03-20 18:22:57'),
+(27, 5, 1, 3453, 4353, 23, 0, '2017-03-20 18:42:53'),
+(28, 5, 2, 0, 0, 23, 0, '2017-03-20 18:22:59');
 
 -- --------------------------------------------------------
 
@@ -53,10 +80,10 @@ INSERT INTO `nodes` (`no.`, `level`, `quality`, `quantity`, `parent`, `level_ind
 
 CREATE TABLE `user` (
   `no.` int(11) NOT NULL,
+  `level` int(255) NOT NULL,
+  `level_index` int(255) NOT NULL,
   `Username` varchar(255) NOT NULL,
   `Password` varchar(255) NOT NULL,
-  `level` int(11) NOT NULL,
-  `level_index` int(11) NOT NULL,
   `position` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -64,10 +91,10 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`no.`, `Username`, `Password`, `level`, `level_index`, `position`) VALUES
-(1, 'admin', 'pass', 1, 1, 2),
-(3, 'paras', 'pass', 2, 2, 0),
-(4, 'supplier', 'supp', 3, 6, 1);
+INSERT INTO `user` (`no.`, `level`, `level_index`, `Username`, `Password`, `position`) VALUES
+(1, 0, 0, 'admin', 'pass', 2),
+(3, 1, 0, 'paras', 'pass', 0),
+(4, 0, 0, 'supplier', 'supp', 1);
 
 --
 -- Indexes for dumped tables
@@ -77,17 +104,13 @@ INSERT INTO `user` (`no.`, `Username`, `Password`, `level`, `level_index`, `posi
 -- Indexes for table `nodes`
 --
 ALTER TABLE `nodes`
-  ADD PRIMARY KEY (`no.`),
-  ADD UNIQUE KEY `level_index` (`level_index`),
-  ADD UNIQUE KEY `level` (`level`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`no.`),
-  ADD UNIQUE KEY `level` (`level`),
-  ADD UNIQUE KEY `level_index` (`level_index`);
+  ADD PRIMARY KEY (`no.`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -97,7 +120,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `nodes`
 --
 ALTER TABLE `nodes`
-  MODIFY `no.` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 --
 -- AUTO_INCREMENT for table `user`
 --
