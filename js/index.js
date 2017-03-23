d0bc135283474436be3ee036af61486aaf889394
Prasanna Dange <prdange25@gmail.com>
2017-03-23 18:15:50 +0530

    added temp function for quantity check

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBZ1nKewxNC_s53gyd-KSdB7gWseVf1Xmg",
@@ -49,11 +48,12 @@ $(document).ready(function(){
 
 for (var i = 1; i <=arr.length; ++i) {
     nodes[i-1] = { "Id": arr[i-1].id, level: arr[i-1].level, level_index:arr[i-1].level_index , quality:arr[i-1].quality , quantity:arr[i-1].quantity , parent:arr[i-1].parent 
-, is_home:arr[i-1].is_home, updated_at:arr[i-1].updated_at  }}
+, is_home:arr[i-1].is_home, updated_at:arr[i-1].updated_at  }
+}
 playersRef.set({
    nodes
    });
-    console.log(playersRef);
+    theft(nodes,arr.length);
             },
             error: function(result) {
           console.log('kjbklj');
@@ -106,4 +106,37 @@ playersRef.set({
         });
    });
 });
- 
+function theft(nodes,length){
+  var sum,i,j;
+  var m=0;
+  var nodes2 = {};
+  var parentids= [];
+  for(i=0;i<length;i++){
+    sum=0;
+    if(parentids.indexOf(nodes[i]["parent"])==-1){
+      parentids[m++]=nodes[i]["parent"];
+      for(j=0;j<length;j++){
+        if(nodes[i]["parent"]==nodes[j]["parent"]){
+          sum=parseInt(sum)+parseInt(nodes[j]["quantity"]);
+        }
+      }
+      nodes2[m]={"parent":nodes[i]["parent"],"sum":sum}
+
+    }
+  }
+  for(var k=1;k<m+1;k++){
+    i=nodes2[k]['parent'];
+    if(i!=-1){
+      console.log(i+"--"+nodes2[k]['sum']+"--"+nodes[i]['quantity']);
+      if(nodes2[k]['sum']!=nodes[i]['quantity']){
+        //the i here will give the id of the meter after which there is a leakage
+
+        console.log("please check line after meter with id:-"+i);
+      }
+    }else{
+      console.log(i+"--"+nodes2[k]['sum']+"--"+nodes[0]['quantity'])
+    }
+  }
+  console.log("after for loop");
+}
+
